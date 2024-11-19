302b279cef23e2123c0d0830caf5351472b8e04c
Kevin <2022023@utsh.edu.mx>
2024-11-19 12:10:01 -0600

    5to intento

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
     
         const passwordValid = await bcrypt.compare(password, user.password);
         if (!passwordValid) {
-            throw new UnauthorizedException("Credenciales inválidas. Verifica tu contraseña");
+            throw new UnauthorizedException("Credenciales inválidas. Verifica tu contraseña tu usuario es: " + user);
         }
     
         const payload = { sub: user.id_user, username: user.name };
